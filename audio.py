9db692a6dcdbeaddb07b21c0219d6cf0fb7d0de1
Brasonite <gabriel.silva.araujo@proton.me>
2025-12-16 11:08:20 -0300

    Feat: Add MP3 support

--- audio.py
+++ audio.py
@@ -1,18 +1,24 @@
-from discord.sinks import Filters, WaveSink
-import discord
+from discord.sinks import Filters, MP3Sink, WaveSink
 import time
+
+
+class TimestampedMP3Sink(MP3Sink):
+    def __init(self, *, filters=None):
+        super().__init__(filters=filters)
+
+        self.start = None
+
+    @Filters.container
+    def write(self, data, user):
+        super().write(data, user)
+
+        if self.start is None:
+            self.start = float(time.time())
 
 
 class TimestampedWaveSink(WaveSink):
     def __init__(self, *, filters=None):
-        if filters is None:
-            filters = discord.sinks.core.default_filters
-        self.filters = filters
-        Filters.__init__(self, **self.filters)
-
-        self.encoding = "wav"
-        self.vc = None
-        self.audio_data = {}
+        super().__init__(filters=filters)
 
         self.start = None
 
